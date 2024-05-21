0092ed83ff8bf90faba0bfee7364c5edeabccca5
Albin <a.folketoft@gmail.com>
2024-05-21 10:26:11 +0200

    blood

--- core/scripts/characters/ZOmbie.gd
+++ core/scripts/characters/ZOmbie.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 @export var speed = 40 
 @export var health = 100
 
+@onready var bloodSFX = $DeathSFX
+
 @export var player: Node2D
 @onready var nav_agent := $NavigationAgent2D as NavigationAgent2D
 
@@ -25,8 +27,12 @@ func take_damage(amount: int) -> void:
 	if health <= 0:
 		var blood_instance = blood.instantiate()
 		get_tree().current_scene.add_child(blood_instance)
+		
+		bloodSFX.play()
+		
 		blood_instance.global_position = global_position
 		blood_instance.rotation = global_position.angle_to_point(player.global_position) - deg_to_rad(180)
+		
 		die()
 
 func die() -> void:
